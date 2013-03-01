e1e43d2573c78680f4853631bdda7b6749612210
kuramass <kuramass@gmail.com>
2013-03-01 12:18:31 +0800

    分頁 & 單一商品顯示功能

---
+++ WineProject/WebContent/product/ProductOne.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=BIG5"
+    pageEncoding="BIG5"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=BIG5">
+<title>${ productVO.p_name }</title>
+<link rel="stylesheet" href="../style/ProductOne.css">
+</head>
+<body>
+<div class=p_content>
+	<div class="p_pics">${ productVO.p_no }圖片</div>
+	<div class="p_data">
+		<div class="p_title">${ productVO.p_name} </div>
+		<ul class="p_info1">
+			<li>${ productVO.p_winery }（${ productVO.p_year }年），${ productVO.p_area }，${ productVO.p_area }。</li>
+			<li>葡萄種類:${ productVO.p_grape }</li>
+			<li>評分：${ productVO.p_rate }</li>
+		</ul>
+		<div class="p_intro">${ productVO.p_intro }</div>
+		<ul>
+			<li class="p_info2">容量：${ productVO.p_vol }</li>
+			<li>酒精濃度:${ productVO.p_alcho }</li>
+		</ul>
+		<div class="p_purchase">
+			<ul>
+			<li>價格：${ productVO.p_price }</li>
+			<li>存貨數量：${ productVO.p_num }</li>
+			</ul>
+		</div>
+	</div>
+	<div class="bar"></div>
+</div>
+
+</body>
+</html>
---
+++ WineProject/src/product/controller/DisplayProducts.java
@@ -0,0 +1,128 @@
+package product.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import product.model.ProductDAO;
+import product.model.ProductVO;
+
+/**
+ * Servlet implementation class DisplayProducts
+ */
+public class DisplayProducts extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DisplayProducts() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String action = request.getParameter("action");
+		
+		if ("getAll".equals(action)){
+			ProductDAO productDAO = new ProductDAO();
+			List<ProductVO> list = productDAO.getAll();
+			HttpSession session = request.getSession();
+			session.setAttribute("list", list);
+			
+			splitPages(list, request);
+			
+			String listAllUrl = "/product/ProductList.jsp";
+			RequestDispatcher rd = request.getRequestDispatcher(listAllUrl);
+			rd.forward(request, response);
+			return;
+		}
+		
+		if ("getOne_For_Display".equals(action)) {
+			Integer p_no = Integer.parseInt(request.getParameter("pId"));
+			ProductDAO productDAO = new ProductDAO();
+			ProductVO productVO = productDAO.findByPrimaryKey(p_no);
+			
+			request.setAttribute("productVO", productVO);
+			String listAllUrl = "/product/ProductOne.jsp";
+			RequestDispatcher rd = request.getRequestDispatcher(listAllUrl);
+			rd.forward(request, response);
+			return;
+		}
+		
+		//Add getSome_For_Display for where xxx=? search. DAO modification is needed. 
+		//JoJo 3/1/2012
+		if ("getSome_For_Display".equals(action)) {
+			String condition = request.getParameter("condition");
+			String conditionValue = request.getParameter("conditionValue");
+			ProductDAO productDAO = new ProductDAO();
+			List<ProductVO> list = productDAO.findByCondition(condition, conditionValue);
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("list", list);
+			
+			splitPages(list, request);
+			
+			String listAllUrl = "/product/ProductList.jsp";
+			RequestDispatcher rd = request.getRequestDispatcher(listAllUrl);
+			rd.forward(request, response);
+			return;
+		}
+	}
+	
+	private void splitPages(List<ProductVO> list, HttpServletRequest request) {
+		int rowsPerPage = 5;  //每頁的筆數    
+	    int totalRows=list.size();      //總筆數
+	    int totalPages=0;     //總頁數      
+	    int whichPage=1;      //第幾頁
+	    int pageIndexArray[]=null;
+	    int pageIndex=0; 
+		
+	    if ((totalRows % rowsPerPage) != 0)
+	    	totalPages = totalRows / rowsPerPage +1;
+	    else
+	    	totalPages = totalRows / rowsPerPage;
+	    
+	    pageIndexArray = new int[totalPages];
+	    for (int i=0; i<totalPages; i++)
+	    	 pageIndexArray[i] = rowsPerPage * i;
+	    //request.getSession().setAttribute("pageIndexArray", pageIndexArray);
+	    
+	    try{
+	    	whichPage = Integer.parseInt(request.getParameter("pageNo"));
+	    	pageIndex = pageIndexArray[whichPage - 1];
+	    } catch (NumberFormatException e){
+	    	whichPage = 1;
+	    	pageIndex = 0;
+	    } catch (ArrayIndexOutOfBoundsException e){
+	    	if (totalPages > 0){
+	    		whichPage = totalPages;
+	    		pageIndex = pageIndexArray[whichPage - 1];
+	    	}
+	    }
+	    
+	    request.getSession().setAttribute("whichPage", whichPage);
+	    request.getSession().setAttribute("pageIndex", pageIndex);
+	    request.getSession().setAttribute("totalPages", totalPages);
+	    request.getSession().setAttribute("totalRows", totalRows);
+	    request.getSession().setAttribute("rowsPerPage", rowsPerPage);
+	}
+
+}
