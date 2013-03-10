f92b76d5611a8595ba1c92da6cbe3a7f0bd20cba
davidou <davidou123@hotmail.com>
2013-03-10 21:50:27 +0800

    [整合] 會員登入整合進主站 [整合] 把每一頁的上方搜尋框丟入模組

--- WineProject/src/login/controller/login.java
+++ WineProject/src/login/controller/login.java
@@ -20,11 +20,12 @@ import member.model.MemberVO;
 @WebServlet("/login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public login() {
-        super();
-    }
 
-	protected void doPost(HttpServletRequest request, 
+	public login() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		MemberHibernateDAO dao = new MemberHibernateDAO();
 		HttpSession session = request.getSession();
@@ -32,48 +33,55 @@ public class login extends HttpServlet {
 		request.setAttribute("ErrorMsgKey", errorMsgs);
 		String m_id = request.getParameter("m_id").trim();
 		String m_pwd = request.getParameter("m_pwd").trim();
-		
-		session.setAttribute("access","n") ;
-		
-		if(m_id != null  &&  m_id.length() != 0 && 
-				m_pwd != null  &&  m_pwd.length() != 0 ){
-			MemberVO member=dao.Login(m_id, m_pwd);
-			
-		if (member != null) {
-			//登入成功狀況
-			session.setAttribute("access","y"); 
-			
-			session.setAttribute("m_id",member.getM_id()); //會員帳號	
-			session.setAttribute("m_no",member.getM_no()); //會員編號
-			session.setAttribute("m_name",member.getM_name()); //會員姓名
-			session.setMaxInactiveInterval(3600) ;
-			System.out.println("success!!") ;
-			response.sendRedirect("accesspage.jsp") ;
+
+		session.setAttribute("access", "n");
+
+		if (m_id != null && m_id.length() != 0 && m_pwd != null
+				&& m_pwd.length() != 0) {
+			MemberVO member = dao.Login(m_id, m_pwd);
+			if (member != null) {
+				// 登入成功狀況
+				session.setAttribute("access", "y");
+
+				session.setAttribute("m_id", member.getM_id()); // 會員帳號
+				session.setAttribute("m_no", member.getM_no()); // 會員編號
+				session.setAttribute("m_name", member.getM_name()); // 會員姓名
+				session.setMaxInactiveInterval(3600);
+				System.out.println("success!!");
+				
+				String redirestpage= null;
+				if(request.getHeader("REFERER").indexOf('?')<0){
+					 redirestpage="?login=y";
+				}
+				else
+				{ redirestpage="&login=y";}
+				response.sendRedirect(request.getHeader("REFERER")+redirestpage);
+//				response.sendRedirect("accesspage.jsp");
+			} else {
+				System.out.println("failed!!");
+				errorMsgs.add("帳號密碼不符，請重新登入");
+				request.setAttribute("ErrorMsgKey", errorMsgs);
+				RequestDispatcher dis = request
+						.getRequestDispatcher("errorLogin.jsp");
+				dis.forward(request, response);
+			}
 		} else {
-			System.out.println("failed!!");
-			errorMsgs.add("帳號密碼不符，請重新登入");
-			request.setAttribute("ErrorMsgKey", errorMsgs);
-			RequestDispatcher dis = request
-					.getRequestDispatcher("errorLogin.jsp"); 
-			dis.forward(request, response);
-		} 
-		}else {
 			errorMsgs.add("帳號或密碼不能為空白");
 			request.setAttribute("ErrorMsgKey", errorMsgs);
 			RequestDispatcher dis = request
-					.getRequestDispatcher("errorLogin.jsp"); 
+					.getRequestDispatcher("errorLogin.jsp");
 			dis.forward(request, response);
 		}
-		
-//		if(request.getParameter("m_id") != null  &&  request.getParameter("m_id").trim().length() != 0 && 
-//		  request.getParameter("m_pwd") != null  &&  request.getParameter("m_pwd").trim().length() != 0 ){
-//			session.setAttribute("access","y") ; 
-//			session.setAttribute("accessid",request.getParameter("m_id")) ;
-//			session.setMaxInactiveInterval(3600) ; 
-//			response.sendRedirect("accesspage.jsp") ;
-//		}else
-//		  System.out.println("密碼帳號不符 !!") ;
+
+		// if(request.getParameter("m_id") != null &&
+		// request.getParameter("m_id").trim().length() != 0 &&
+		// request.getParameter("m_pwd") != null &&
+		// request.getParameter("m_pwd").trim().length() != 0 ){
+		// session.setAttribute("access","y") ;
+		// session.setAttribute("accessid",request.getParameter("m_id")) ;
+		// session.setMaxInactiveInterval(3600) ;
+		// response.sendRedirect("accesspage.jsp") ;
+		// }else
+		// System.out.println("密碼帳號不符 !!") ;
 	}
 }
-
-
