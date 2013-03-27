d1b1e67bce0e3d45b300ff0d2a7cf134db8a291e
yangsanta <a99843576@yahoo.com.tw>
2013-03-27 16:51:47 +0800

    購物車買A送B BUG修正

--- WineProject/src/shoppingCart/controller/ShipingCart.java
+++ WineProject/src/shoppingCart/controller/ShipingCart.java
@@ -35,65 +35,21 @@ public class ShipingCart {
 			// 原有的數量：OldShoppingProduct.getProductNumber()
 
 			// 如果新加入的商品為買A送B的B商品，若用戶原已選購B商品，且贈送的B商品數量>=原購買的B商品數量，則扣抵原購B商品的價格（而非直接增加B商品的數量）
-			if (shoppingProduct.getSaleType().equals("B")) {
-				OldShoppingProduct
-						.setSalesNumber(shoppingProduct.getSalesNumber()
-								+ OldShoppingProduct.getSalesNumber());
-				AbDAO abdao = new AbDAO();
-				AbVO abVO = abdao.findByBKey(productNO);
-				Integer a_no = abVO.getAb_a_p_id();
-				ShoppingProduct OldShoppingProductA = cart.get(a_no);
-	
-				if (OldShoppingProductA == null) {
-					
-					OldShoppingProduct.setProductNumber(shoppingProduct
-							.getProductNumber()
-							+ OldShoppingProduct.getProductNumber());
-					// 計算小計
-					OldShoppingProduct.setSubTotal(shoppingProduct
-							.getSubTotal() + OldShoppingProduct.getSubTotal());
-				} else {
-					if ((OldShoppingProduct.getSubTotal() != 0)
-							&& (shoppingProduct.getProductNumber() >= OldShoppingProductA
-									.getProductNumber())) {
-						OldShoppingProduct.setProductNumber(shoppingProduct
-								.getProductNumber());
-						OldShoppingProduct.setSubTotal(shoppingProduct
-								.getSubTotal());
-					} else if ((OldShoppingProduct.getSubTotal() != 0)
-							&& (shoppingProduct.getProductNumber() < OldShoppingProductA
-									.getProductNumber())) {
-						OldShoppingProduct.setProductNumber(OldShoppingProduct
-								.getProductNumber());
-						OldShoppingProduct.setSubTotal(OldShoppingProduct
-								.getSubTotal()
-								- shoppingProduct.getProductPrice()
-								* shoppingProduct.getProductNumber());
-					} else {
-						OldShoppingProduct.setProductNumber(shoppingProduct
-								.getProductNumber()
-								+ OldShoppingProduct.getProductNumber());
-						// 計算小計
-						OldShoppingProduct.setSubTotal(shoppingProduct
-								.getSubTotal()
-								+ OldShoppingProduct.getSubTotal());
-					}
-				}
-				// 此外則照原規則
-			} else {
+			
 				OldShoppingProduct.setProductNumber(shoppingProduct
 						.getProductNumber()
 						+ OldShoppingProduct.getProductNumber());
 				// 計算小計
 				OldShoppingProduct.setSubTotal(shoppingProduct.getSubTotal()
 						+ OldShoppingProduct.getSubTotal());
-			}
+			
 
 			// 如果新加入的商品B為買A送B的結果，SalesNumber往上加，以便確認訂單頁面判斷是否顯示已折扣
 
 		}
 	}
 
+	
 	// 計算總商品總價
 	public Integer getTotal() {
 		Integer total = 0;
