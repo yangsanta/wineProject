182fd5d1ad9b6f139f63330852be3f86082f8f0c
kuramass <kuramass@gmail.com>
2013-03-29 19:52:28 +0800

    Coupon_set & Shipping_set 後台增加javascript驗證

--- WineProject/src/shipping_set/controller/Shipping_setServlet.java
+++ WineProject/src/shipping_set/controller/Shipping_setServlet.java
@@ -37,51 +37,9 @@ public class Shipping_setServlet extends HttpServlet {
 		shipping_setVO2.setMin_pirce(new Integer(min_pirce));
 
 		dao.update(shipping_setVO2);
-
-		RequestDispatcher rd = request
-				.getRequestDispatcher("/wine_admin/ademin_Shipping.jsp");
-		rd.forward(request, response);
-
-		// response.sendRedirect("/WineProject/wine_admin/ademin_Fs_Success.jsp");
-
-		return;
-
-		// List<String> errorMsgs = new LinkedList<String>();
-		// // Store this set in the request scope, in case we need to
-		// // send the ErrorPage view.
-		// request.setAttribute("errorMsgs", errorMsgs);
-		//
-		// try {
-		// /*************************** 1.接收請求參數 - 輸入格式的錯誤處理
-		// **********************/
-		// Integer min_pirce = new
-		// Integer(request.getParameter("min_pirce").trim());
-		//
-		// Shipping_setVO shipping_setVO = new Shipping_setVO();
-		// shipping_setVO.setMin_pirce(min_pirce);
-		//
-		// /*************************** 2.開始修改資料
-		// *****************************************/
-		// Shipping_setService empSvc = new Shipping_setService();
-		// shipping_setVO = empSvc.updateEmp(min_pirce);
-		//
-		// /*************************** 3.修改完成,準備轉交(Send the Success view)
-		// *************/
-		// request.setAttribute("shipping_setVO", shipping_setVO); //
-		// 資料庫update成功後,正確的的empVO物件,存入req
-		// String url = "/emp/listOneEmp.jsp";
-		// RequestDispatcher successView = request.getRequestDispatcher(url); //
-		// 修改成功後,轉交listOneEmp.jsp
-		// successView.forward(request, response);
-		//
-		// /*************************** 其他可能的錯誤處理
-		// *************************************/
-		// } catch (Exception e) {
-		// errorMsgs.add("修改資料失敗:" + e.getMessage());
-		// RequestDispatcher failureView = request
-		// .getRequestDispatcher("/emp/update_emp_input.jsp");
-		// failureView.forward(request, response);
-		// }
+		
+		response.sendRedirect("/WineProject/couponset/couponsetadmin.do?action=getAll");
+		
 	}
 
 }
